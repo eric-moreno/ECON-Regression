1de5e2e100019a291f28f28a0a44283b5f5f3344
eric-moreno <emoreno@mit.edu>
2022-02-01 16:45:23 -0600

    new training restructure

---
+++ Trainings/eval.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import os
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import keras 
+#import setGPU
+from numpy.random import seed
+import tensorflow as tf
+from model import model_ConvDNN, model_DNN
+from keras import regularizers, Sequential 
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.losses import mean_absolute_error, MeanAbsoluteError, mean_squared_error, MeanSquaredError
+
+model_outdir = 'DNN_v1'
+model_name = 'best_model_DNN'
+model_type = 'DNN' 
+
+#Load train data
+train_list = [pd.read_hdf('../Preprocessing/train_0-20k.h5'), pd.read_hdf('../Preprocessing/train_20-40k.h5')]
+train_data = pd.concat(train_list).to_numpy().reshape((40000, 800, 6))#[:, :, :, -2:]
+truth = pd.read_hdf('../Preprocessing/truths.h5').to_numpy()[:40000, 3]
+
+#Predict train data
+model = keras.models.load_model('%s/%s'%(model_outdir,model_name))
+pred = model.predict(train_data)
+np.save('%s/prediction_training_0-40k.npy'%(model_outdir), pred) 
+
+#Load test data
+test_list = [pd.read_hdf('../Preprocessing/train_40-48k.h5')]
+test_data = pd.concat(test_list).to_numpy().reshape((8000, 800, 6))
+test_truth = pd.read_hdf('../Preprocessing/truths.h5').to_numpy()[40000:48000,3]
+
+
+mse = MeanSquaredError(reduction='none')
+
+#Loss distribution for naive simenergy sum calculation
+mse_distrib = mse(test_truth.reshape(-1, 1), test_data.reshape(8000, 800, 6)[:,:, -1].sum(axis=1).reshape(-1, 1)/16).numpy()
+print(mse_distrib)
+plt.figure()
+plt.title('Test Loss Distribution')
+plt.ylabel('Count')
+plt.xlabel('MSE Loss')
+n, bins, patches = plt.hist(mse_distrib, 30, facecolor='blue', label='Sum(SimEnergy)', alpha=0.5, range=(0, 6000))
+
+#Predict test data
+pred = model.predict(test_data) 
+np.save('%s/prediction_test_40-48k.npy'%(model_outdir), pred) 
+
+#Loss distribution from model 
+mse_distrib = mse(test_truth.reshape(-1, 1), pred.reshape(-1,1)).numpy()
+n, bins, patches = plt.hist(mse_distrib, 30, facecolor='red',label='%s model'%(model_type), alpha=0.5, range=(0, 6000))
+plt.legend()
+plt.savefig('%s/mse_distrib_sumsimenergy.jpg'%(model_outdir))
+
+
--- Trainings/training.py
+++ Trainings/training.py
@@ -5,35 +5,20 @@ import matplotlib
 import seaborn as sns
 import os
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import setGPU
 import keras 
-#import setGPU
 from numpy.random import seed
 import tensorflow as tf
-#tf.logging.set_verbosity(tf.logging.ERROR) 
-
-from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVector, Conv1D, MaxPooling2D, UpSampling1D, BatchNormalization, Flatten, Reshape, GRU, Conv2D, Bidirectional, Concatenate
-from keras.models import Model
+from model import model_ConvDNN, model_DNN
 from keras import regularizers, Sequential 
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.losses import mean_absolute_error, MeanAbsoluteError, mean_squared_error, MeanSquaredError
 
 train_list = [pd.read_hdf('../Preprocessing/train_0-20k.h5'), pd.read_hdf('../Preprocessing/train_20-40k.h5')]
-train_data = pd.concat(train_list).to_numpy().reshape((40000, 800, 6))
-truth = pd.read_hdf('../Preprocessing/truths.h5').to_numpy()[:40000,3]
-print(train_data.shape)
-
-print(train_data[:,:, -1].sum(axis=1)/16) 
+train_data = pd.concat(train_list).to_numpy().reshape((40000, 800, 6))#[:, :, :, -2:]
+truth = pd.read_hdf('../Preprocessing/truths.h5')
+truth = truth.to_numpy()[:40000, 3]
 print(truth) 
-
-mse = MeanSquaredError(reduction='none')
-mse_distrib = mse(truth.reshape(-1, 1), train_data[:,:, -1].sum(axis=1).reshape(-1, 1)/16).numpy()
-print(mse_distrib)
-plt.title('Loss distribution Sum(Simenergy)')
-plt.xlabel('Count')
-plt.ylabel('Loss')
-plt.figure()
-n, bins, patches = plt.hist(mse_distrib, 10, facecolor='blue', alpha=0.5, range=(0, 2000))
-plt.savefig('mse_distrib_sumsimenergy.jpg')
 
 #scaler = StandardScaler()
 #truth = scaler.fit_transform(truth.reshape(-1, 1))
@@ -41,53 +26,26 @@ plt.savefig('mse_distrib_sumsimenergy.jpg')
 #print(train_data.to_numpy().reshape((1000, 800, 6)))
 #prerint(truth.to_numpy()[:,3])
 
-def model_ConvDNN(X):
-    inputs = Input(shape=(X.shape[1],X.shape[2], X.shape[3]))
-    L1 = Conv2D(32, (2, 2), activation="relu")(inputs)
-    L2 = MaxPooling2D((2,2))(L1)
-    print(L2.shape)
-    L3 = Conv2D(16, (2, 2), activation="relu")(L2)
-    print(L3.shape)
-    L4 = MaxPooling2D((2,2))(L3)
-    print(L4.shape)
-    L5 = Conv2D(8, (2, 2), activation="relu")(L4)
-    L6 = MaxPooling2D((2,2))(L5)
-    x = Flatten()(L6)
-    x = Dense(32, activation='relu')(x)
-    output = Dense(1, activation='relu')(x)
-    model = Model(inputs=inputs, outputs = output)
-    return model  
-
-def model_DNN(X): 
-    inputs = Input(shape=(X.shape[1],X.shape[2]))
-    x = Dense(1, activation='relu')(inputs)
-    x = Flatten()(x)
-    x = Dense(256, activation='relu')(x)
-    x = Dense(64, activation='relu')(x)
-    output = Dense(1, activation='relu')(x)
-    model = Model(inputs=inputs, outputs=output)
-    return model
-
 
 model = model_DNN(train_data) 
 model.compile(optimizer='adam', loss='mse')
 model.summary()
-outdir = '.'
+outdir = 'DNN_v1'
 model_type = "DNN" 
 # fit the model to the data
 nb_epochs = 100
 batch_size = 16
 earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
-mcp_save = ModelCheckpoint('%s/best_model_%s.hdf5'%(outdir, model_type), save_best_only=True, monitor='val_loss', mode='min')
+mcp_save = ModelCheckpoint('%s/best_model_%s'%(outdir, model_type), save_best_only=True, monitor='val_loss', mode='min')
 history = model.fit(train_data, truth, epochs=nb_epochs, batch_size=batch_size,
                     validation_split=0.2, callbacks=[earlyStopping, mcp_save]).history
-model.save('%s/last_model.hdf5'%(outdir))
+model.save('%s/last_model_%s'%(outdir, model_type))
 
 fig = plt.figure()
-plt.plot(range(nb_epochs),history['loss'],label='training loss', color = "blue")
-plt.plot(range(nb_epochs),history['val_loss'],label='validation loss', color = "red")
+plt.plot(history['loss'][1:],label='training loss', color = "blue")
+plt.plot(history['val_loss'][1:],label='validation loss', color = "red")
 plt.xlabel('epochs')
 plt.ylabel('MSE Loss')
+plt.legend()
 plt.title("Training and validation loss")
 fig.savefig('loss.jpg')
-
