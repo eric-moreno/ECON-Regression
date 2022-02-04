1b9d2ab48442953572d9d361a964747997b54e37
eric-moreno <emoreno@mit.edu>
2022-02-04 00:29:37 -0600

    updated training/eval to ECON data

--- Trainings/eval.py
+++ Trainings/eval.py
@@ -14,28 +14,31 @@ from keras import regularizers, Sequential
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.losses import mean_absolute_error, MeanAbsoluteError, mean_squared_error, MeanSquaredError
 
-model_outdir = 'DNN_v1'
+model_outdir = 'DNN_v1_onlywafers'
 model_name = 'best_model_DNN'
 model_type = 'DNN' 
 
 #Load train data
-train_list = [pd.read_hdf('../Preprocessing/train_0-20k.h5'), pd.read_hdf('../Preprocessing/train_20-40k.h5')]
-train_data = pd.concat(train_list).to_numpy().reshape((40000, 800, 6))#[:, :, :, -2:]
-truth = pd.read_hdf('../Preprocessing/truths.h5').to_numpy()[:40000, 3]
+train_list = pd.read_hdf('../Preprocessing/data_0-48k.h5')
+train_list.drop(columns=['index'], inplace=True)
+train_data = train_list.to_numpy().reshape((48000, 50, 16))[:40000]
+test_data = train_list.to_numpy().reshape((48000, 50, 16))[40000:]
+truth = pd.read_hdf('../Preprocessing/truths_0-48k.h5')
+train_truth = truth.to_numpy()[:40000, 3]
+test_truth = truth.to_numpy()[40000:, 3]
 
 #Predict train data
 model = keras.models.load_model('%s/%s'%(model_outdir,model_name))
 pred = model.predict(train_data)
 np.save('%s/prediction_training_0-40k.npy'%(model_outdir), pred) 
 
-#Load test data
-test_list = [pd.read_hdf('../Preprocessing/train_40-48k.h5')]
-test_data = pd.concat(test_list).to_numpy().reshape((8000, 800, 6))
-test_truth = pd.read_hdf('../Preprocessing/truths.h5').to_numpy()[40000:48000,3]
-
+#Predict test data
+pred = model.predict(test_data) 
+np.save('%s/prediction_test_40-48k.npy'%(model_outdir), pred) 
 
 mse = MeanSquaredError(reduction='none')
 
+'''
 #Loss distribution for naive simenergy sum calculation
 mse_distrib = mse(test_truth.reshape(-1, 1), test_data.reshape(8000, 800, 6)[:,:, -1].sum(axis=1).reshape(-1, 1)/16).numpy()
 plt.figure()
@@ -43,14 +46,11 @@ plt.title('Test Loss Distribution')
 plt.ylabel('Count')
 plt.xlabel('MSE Loss')
 n, bins, patches = plt.hist(mse_distrib, 30, facecolor='blue', label='Sum(SimEnergy)', alpha=0.5, range=(0, 6000))
-
-#Predict test data
-pred = model.predict(test_data) 
-np.save('%s/prediction_test_40-48k.npy'%(model_outdir), pred) 
+'''
 
 #Loss distribution from model 
 mse_distrib = mse(test_truth.reshape(-1, 1), pred.reshape(-1,1)).numpy()
-n, bins, patches = plt.hist(mse_distrib, 30, facecolor='red',label='%s model'%(model_type), alpha=0.5, range=(0, 6000))
+n, bins, patches = plt.hist(mse_distrib, 30, facecolor='red',label='%s model'%(model_type), alpha=0.5, range=(0, 60000))
 plt.legend()
 plt.savefig('%s/mse_distrib_sumsimenergy.jpg'%(model_outdir))
 
--- Trainings/model.py
+++ Trainings/model.py
@@ -1,18 +1,34 @@
 import numpy as np
 import keras 
 import tensorflow as tf
-from keras.layers import Input, Dense, MaxPooling2D, BatchNormalization, Flatten, Reshape, Conv2D
+from keras.layers import Input, Dense, MaxPooling2D, BatchNormalization, Flatten, Reshape, Conv2D, Conv1D, MaxPooling1D
 from keras.models import Model
 from keras import regularizers, Sequential 
 
 def model_ConvDNN(X):
-    inputs = Input(shape=(X.shape[1], X.shape[2],X.shape[3]))
-    L1 = Conv2D(4, (2, 2), activation="relu")(inputs)
-    L2 = MaxPooling2D((2,2))(L1)
-    L3 = Conv2D(8, (2, 2), activation="relu")(L2)
-    L4 = MaxPooling2D((2,2))(L3)
-    L5 = Conv2D(16, (2, 2), activation="relu")(L4)
-    L6 = MaxPooling2D((2,2))(L5)
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv2D(4, 2, activation="relu")(inputs)
+    L2 = MaxPooling2D(2)(L1)
+    L3 = Conv2D(8, 2, activation="relu")(L2)
+    L4 = MaxPooling2D(2)(L3)
+    L5 = Conv2D(16, 2, activation="relu")(L4)
+    L6 = MaxPooling2D(2)(L5)
+    x = Flatten()(L6)
+    x = Dense(128, activation='relu')(x)
+    x = Dense(32, activation='relu')(x)
+    output = Dense(1, activation='relu')(x)
+    model = Model(inputs=inputs, outputs = output)
+    return model  
+
+
+def model_1dConvDNN(X):
+    inputs = Input(shape=(X.shape[1], X.shape[2]))
+    L1 = Conv1D(16, 2, activation="relu")(inputs)
+    L2 = MaxPooling1D(2)(L1)
+    L3 = Conv1D(32, 2, activation="relu")(L2)
+    L4 = MaxPooling1D(2)(L3)
+    L5 = Conv1D(64, 2, activation="relu")(L4)
+    L6 = MaxPooling1D(2)(L5)
     x = Flatten()(L6)
     x = Dense(128, activation='relu')(x)
     x = Dense(32, activation='relu')(x)
@@ -22,7 +38,7 @@ def model_ConvDNN(X):
 
 def model_DNN(X): 
     inputs = Input(shape=(X.shape[1],X.shape[2]))
-    x = Dense(2, activation='relu')(inputs)
+    x = Dense(8, activation='relu')(inputs)
     x = Flatten()(x)
     x = Dense(256, activation='relu')(x)
     x = Dense(64, activation='relu')(x)
@@ -31,4 +47,13 @@ def model_DNN(X):
     model = Model(inputs=inputs, outputs=output)
     return model
 
+def model_DNN_flatten(X): 
+    inputs = Input(shape=(X.shape[1],X.shape[2]))
+    x = Flatten()(inputs)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dense(16, activation='relu')(x)
+    output = Dense(1, activation='relu')(x)
+    model = Model(inputs=inputs, outputs=output)
+    return model
 
